833ff2075a28cc83142b12d52b0da20fc1d6289e
Hendri Karisma <situkangsayur@gmail.com>
2019-07-27 11:49:30 +0700

    creating testcase

---
+++ tests/test_hashicorp.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest import mock
+
+class TestHashicorp(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_config_dict(self):
+        pass
+
+    def test_consul_init(self):
+        pass
+
+    def test_consul_get_kv(self):
+        pass
+
+    def test_vault_init(self):
+        pass
+
+    def test_vault_get_kv(self):
+        pass
---
+++ tests/test_healthcheck.py
@@ -0,0 +1,7 @@
+import unittest
+from unittest import mock
+
+class TestHealtchcheck(unittest.TestCase):
+
+    def setUp(self):
+        pass
