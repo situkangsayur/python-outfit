802227a867fa20f2e3308255f1d7bf1fdbf8c43f
Hendri Karisma <situkangsayur@gmail.com>
2019-07-30 17:50:51 +0700

    add docs

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. python-cloud documentation master file, created by
+   sphinx-quickstart on Tue Jul 30 17:46:35 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to python-cloud's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -10,10 +10,16 @@ from pycloud.utils.logger import Logger
 class BasicHealthCheckTest(unittest.TestCase):
 
     def setUp(self):
-        self.path = '/healtcheck'
+        self.path = '/healthcheck'
+        self.path2 = '/healthcheck_200'
         self.app = flask.Flask(__name__)
+        self.app2 = flask.Flask(__name__)
+
         self.hc = HealthCheck()
+        self.hc2 = HealthCheck()
+
         self.client = self.app.test_client()
+        self.client2 = self.app2.test_client()
         dbmongo = None
         service_enum = {
             'mongoengine': {
@@ -23,25 +29,32 @@ class BasicHealthCheckTest(unittest.TestCase):
         }
         
         self.hc = HealthcheckList.setup(healthcheck = self.hc, service_list = service_enum)
+
         self.app.add_url_rule(self.path, view_func=lambda: self.hc.run())
+        self.app2.add_url_rule(self.path2, view_func=lambda: self.hc2.run())
+
+
+
+    def test_basic_check_success(self):
+        response = self.client2.get(self.path2)
+        self.assertEqual(200, response.status_code)
 
 
     def test_basic_check(self):
         response = self.client.get(self.path)
         Logger.info(response)
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(500, response.status_code)
 
     def test_failing_check(self):
         def fail_check():
             return False, "FAIL"
 
-        self.hc.add_check(fail_check)
-        response = self.client.get(self.path)
+        self.hc2.add_check(fail_check)
+        response = self.client2.get(self.path2)
         self.assertEqual(500, response.status_code)
 
         respon = flask.json.loads(response.data)
         self.assertEqual("failure", respon["status"])
-
 
 if __name__ == '__main__':
     unittest.main()
